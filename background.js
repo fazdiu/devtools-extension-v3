d33bacd94caaef296dcf8fed2ff7731373a7c3a5
Fernando R <dev.fernandocf@gmail.com>
2023-11-22 18:18:48 -0500

    initial

---
+++ background.js
@@ -0,0 +1,34 @@
+// Chrome automatically creates a background.html page for this to execute.
+// This can access the inspected page via executeScript
+
+chrome.runtime.onConnect.addListener(function (port) {
+  var extensionListener = function (message, sender, sendResponse) {
+    if (message.tabId) {
+      //Evaluate script in inspectedPage
+      //Pass message to inspectedPage
+      // console.log("to current tab");
+      chrome.tabs.sendMessage(message.tabId, message, sendResponse);
+    
+    } else {
+      // This accepts messages from the inspectedPage and
+      // sends them to the panel
+      // console.log("to panel");
+      port.postMessage(message);
+    }
+    sendResponse(message);
+  };
+
+  // Listens to messages sent from the panel
+  chrome.runtime.onMessage.addListener(extensionListener);
+
+  port.onDisconnect.addListener(function (port) {
+    chrome.runtime.onMessage.removeListener(extensionListener);
+  });
+
+  // port.onMessage.addListener(function (message) {
+  //     port.postMessage(message);
+  // });
+});
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  return true;
+});
---
+++ content.js
@@ -0,0 +1,27 @@
+console.log("content.js loaded");
+
+const btn = document.createElement("btn");
+btn.innerHTML = "click send panel";
+(document.body || document.documentElement).appendChild(btn);
+
+btn.addEventListener("click", () => {
+  console.log('click');
+  sendObjectToDevTools({ content: "Changed by page" });
+});
+
+/**
+ * Permite:
+ * 1. Send messages/data to devtools panel
+ * 2. Receive messages/data from the panel   
+ */
+
+// 1
+function sendObjectToDevTools(message) {
+  // The callback here can be used to execute something on receipt
+  chrome.runtime.sendMessage(message, function (message) {});
+}
+// 2
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  sendResponse({ status: "ok" });
+  console.log("message from panel", message);
+});
